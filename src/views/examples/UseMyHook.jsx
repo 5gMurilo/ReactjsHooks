dc240ab4ece10a0c97a598f518cb48cc296f9baf
Murilo Cardoso <65872392+5gMurilo@users.noreply.github.com>
2021-02-10 15:58:10 -0300

    commit

--- src/views/examples/UseMyHook.jsx
+++ src/views/examples/UseMyHook.jsx
@@ -2,15 +2,26 @@ import PageTitle from "../../components/layout/PageTitle";
 import SectionTitle from "../../components/layout/SectionTitle";
 
 import { UseCounter } from "../../Hooks/UseCounter";
+import { useFetch } from "../../Hooks/useFetch";
 
 const UseMyHook = () => {
-  const [count, inc, dec] = UseCounter();
+  const url = "http://files.cod3r.com.br/curso-react/estados.json";
 
+  const [count, inc, dec] = UseCounter();
+  const response = useFetch(url);
+
+  function showStates(states) {
+    return states.map((state) => (
+      <li key={state.nome} className="list">
+        {state.nome} - {state.sigla}
+      </li>
+    ));
+  }
   return (
     <>
       <PageTitle title="My Hook" subtitle="Hook criado por mim" />
 
-      <SectionTitle title="Exercício #01" />
+      <SectionTitle title="Exercício #01 - mudando estado com incremento e decremento de numeros" />
       <div className="center">
         <span className="text">{count}</span>
 
@@ -22,6 +33,13 @@ const UseMyHook = () => {
             decremento
           </button>
         </div>
+      </div>
+
+      <SectionTitle title="Exercício #02 - carregando dados da url" />
+      <div className="center">
+        <ul className="ul">
+          {response.data ? showStates(response.data) : false}
+        </ul>
       </div>
     </>
   );
